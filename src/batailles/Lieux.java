49e9513be3e98e441dc4b04f240f06a805e88658
Belgnaoui Othman <belgnaoui.othman@gmail.com>
2024-02-27 17:21:43 +0100

    Ajouté batailles, conteur, et interface Lieu pour dynamiser interactions

---
+++ src/batailles/Lieux.java
@@ -0,0 +1,5 @@
+package batailles;
+
+public enum Lieux {
+
+}
---
+++ src/personnages/Conteur.java
@@ -0,0 +1,7 @@
+package personnages;
+
+public class Conteur {
+    public void raconter(Bataille bataille) {
+        System.out.println("Je raconte");
+    }
+}
